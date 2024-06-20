c990e530a08c87529415d20cafe8a240f56470d4
chaewon <cwlee4633@naver.com>
2024-06-20 13:57:25 +0900

    HTTP 요청 - @ModelAttribute

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,16 +1,17 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.Objects;
 
 @Slf4j
 @Controller
@@ -81,4 +82,32 @@ public class RequestParamController {
         return "ok";
     }
 
+//    @ResponseBody
+//    @RequestMapping("model-attribute-v1")
+//    public String modelAttribute(@RequestParam String username, @RequestParam int age){
+//
+//        HelloData helloData = new HelloData();
+//        helloData.setUsername(username);
+//        helloData.setAge(age);
+//
+//        log.info("username={}, age={}",helloData.getUsername(), helloData.getAge());
+//
+//        //@Date 에 포함된 ToString 이 자동으로 만들어준다.
+//        log.info("helloData={}", helloData);
+//        return "ok";
+//    }
+
+    @ResponseBody
+    @RequestMapping("model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData){
+        log.info("username={}, age={}",helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData){
+        log.info("username={}, age={}",helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
 }
